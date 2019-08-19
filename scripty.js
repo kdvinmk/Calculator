8bec820954e8aeca80879cb57cf15c84abdf9e97
Kavin Mukil <kdvinmk@gmail.com>
2019-08-20 00:41:16 +0530

    Add the following

---
+++ scripty.js
@@ -0,0 +1,26 @@
+function add(a,b){
+    let c = a+b;
+    console.log(c);
+}
+
+function sub(a,b){
+    let c = a-b;
+    console.log(c);
+
+}
+
+function divide(a,b){
+
+    let c = a/b;
+    console.log(c);
+}
+
+function multiply(a,b){
+    let c = a*b;
+    console.log(c);
+}
+
+add(4,6);
+sub(4,6);
+multiply(4,6);
+divide(4,6);
